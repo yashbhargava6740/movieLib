fc390ccf51618859922a654c6accc9da61891981
ncoder <bhargavayash2015@gmail.com>
2024-05-31 23:10:18 +0530

    added background image

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <div>
+    <div 
+    className="bg-cover bg-center min-h-screen"
+    style={{ backgroundImage: `url('/img.jpg')` }}
+    >
     <App />
     <Toaster/>
     </div>
