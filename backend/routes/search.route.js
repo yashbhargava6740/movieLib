886ef7cb1c7baeeedaae3800977c6f177a508258
ncoder <bhargavayash2015@gmail.com>
2024-06-02 17:07:26 +0530

    bug fix

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const SearchController = require('../controllers/search.controller');
 const router = express.Router();
-router.get('/', SearchController.getSearchedMovie);
 router.get('/find/:id', SearchController.getMovieById);
 router.get('/playlists',  SearchController.fetchAllPublicPlaylists)
+router.get('/', SearchController.getSearchedMovie);
 module.exports = router;
